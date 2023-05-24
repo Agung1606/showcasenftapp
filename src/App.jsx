5ccf290b3640869d734213fbb60c185d425808dc
agungsaputra <agngsptra928@gmail.com>
2023-05-24 09:48:01 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { SectionWrapper, Download, Features } from "./components";
 import assets from "./assets";
+import styles from "./styles/Global";
 
 const App = () => {
   return (
@@ -30,6 +31,13 @@ const App = () => {
         mockupImg={assets.mockup}
         banner="banner02"
       />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by{" "}
+          <span className="bold">Agung Saputra x Javascript Mastery</span>
+        </p>
+      </div>
     </>
   );
 };
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+
+import styles from "../styles/Global";
+import assets from "../assets";
 
 const Download = () => {
+  const goToSourceCode = () =>
+    window.open("https://github.com/Agung1606/NFTsApp", "_blank");
   return (
-    <div>Download</div>
-  )
-}
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on Github
+          </p>
+        </div>
+        <button onClick={goToSourceCode} className={styles.btnPrimary}>
+          Source Code
+        </button>
+        <div className={styles.flexCenter}>
+          <img
+            src={assets.scene}
+            alt="download-png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Download
+export default Download;
